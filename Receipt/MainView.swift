665f17104d80544bd43b1ee5a78d7a3c88da9b77
Ben-Langlois-1 <benlanglois@proton.me>
2026-02-14 16:32:18 -0500

    Added example of graph view and AI chatbot on home screen, spending screen to add a spending event (not functional), and interest calculator.

--- Receipt/MainView.swift
+++ Receipt/MainView.swift
@@ -8,21 +8,26 @@
 import SwiftUI
 
 struct MainView: View {
+
     var body: some View {
+
         TabView {
             Tab("Home", systemImage: "house") {
-                HomeScreen()
+                HomeView()
             }
             Tab("Goals", systemImage: "flag.pattern.checkered") {
                 GoalsView()
             }
+            Tab("Tools", systemImage: "wrench.and.screwdriver.fill") {
+                ToolsView()
+            }
             Tab("Settings", systemImage: "gear") {
                 SettingsView()
             }
+
         }
     }
 }
-
 #Preview {
     MainView()
 }
